471da9ac11cf7cf5a1e618f06ee015f0cc4b46f0
HenryJrA <henryjrallam7@gmail.com>
2022-03-19 13:23:10 -0400

    init with sanity

---
+++ share_backend/schemas/user.js
@@ -0,0 +1,18 @@
+export default{
+  nme:'user',
+  title:'User',
+  type:'document',
+  fields:[
+    {
+      name:'userName',
+      title:'UserName',
+      type:'string',
+
+    },
+    {
+      name:'image',
+      title:'Iamge',
+      type:'string'
+    },
+  ]
+}
